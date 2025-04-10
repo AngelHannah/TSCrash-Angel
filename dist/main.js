b0690135b88fd164d2b43ee9c521ffe2d290978f
Hannah <87335534+AngelHannah@users.noreply.github.com>
2025-04-09 23:13:22 -0700

    Edited main

--- dist/main.js
+++ dist/main.js
@@ -1,2 +1,15 @@
 var a = "1";
 console.log("aaa", a);
+//Variables in typescript
+//constants are not allowed to change in ts
+var hello = "world";
+//In ts we can only assign data of the same type to the variable, to ensure the type will never change
+var helloagain = "world";
+//Syntax to explicity specify the type of a variable
+var hellothere = "world";
+//Functions in TS
+var getFullName = function (name, surname) {
+    //Arrow functions usually easier and faster
+    return name + " " + surname;
+};
+console.log(getFullName("Hannah", "Angel"));
--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,21 @@
 const a = "1";
 
 console.log("aaa", a);
+
+//Variables in typescript
+//constants are not allowed to change in ts
+const hello = "world";
+
+//In ts we can only assign data of the same type to the variable, to ensure the type will never change
+let helloagain = "world";
+
+//Syntax to explicity specify the type of a variable
+let hellothere: string = "world";
+
+//Functions in TS
+const getFullName = (name: string, surname: string) => {
+    //Arrow functions usually easier and faster
+    return name + " " + surname;
+}
+
+console.log(getFullName("Hannah", "Angel"));
